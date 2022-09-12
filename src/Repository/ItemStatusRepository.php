ca71c368ff656c3ed7c58049e9625fa5a3a155b3
grodeklukasz <lukasz.grodek@gmail.com>
2022-09-12 13:42:42 +0200

    add ItemStatus Entity, create Migration

---
+++ src/Repository/ItemStatusRepository.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\ItemStatus;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<ItemStatus>
+ *
+ * @method ItemStatus|null find($id, $lockMode = null, $lockVersion = null)
+ * @method ItemStatus|null findOneBy(array $criteria, array $orderBy = null)
+ * @method ItemStatus[]    findAll()
+ * @method ItemStatus[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class ItemStatusRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, ItemStatus::class);
+    }
+
+    public function add(ItemStatus $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->persist($entity);
+
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+
+    public function remove(ItemStatus $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->remove($entity);
+
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+
+//    /**
+//     * @return ItemStatus[] Returns an array of ItemStatus objects
+//     */
+//    public function findByExampleField($value): array
+//    {
+//        return $this->createQueryBuilder('i')
+//            ->andWhere('i.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->orderBy('i.id', 'ASC')
+//            ->setMaxResults(10)
+//            ->getQuery()
+//            ->getResult()
+//        ;
+//    }
+
+//    public function findOneBySomeField($value): ?ItemStatus
+//    {
+//        return $this->createQueryBuilder('i')
+//            ->andWhere('i.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->getQuery()
+//            ->getOneOrNullResult()
+//        ;
+//    }
+}
