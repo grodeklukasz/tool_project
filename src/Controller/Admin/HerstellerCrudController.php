07edc7bc97682494aa5f55ce6103e76d20c8504a
grodeklukasz <lukasz.grodek@gmail.com>
2022-09-20 11:02:10 +0200

    disable delete for Producer (Hersteller)

--- src/Controller/Admin/HerstellerCrudController.php
+++ src/Controller/Admin/HerstellerCrudController.php
@@ -4,6 +4,8 @@ namespace App\Controller\Admin;
 
 use App\Entity\Hersteller;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
 
 class HerstellerCrudController extends AbstractCrudController
 {
@@ -12,6 +14,10 @@ class HerstellerCrudController extends AbstractCrudController
         return Hersteller::class;
     }
 
+    public function configureActions(Actions $actions): Actions 
+    {
+        return $actions->disable(Action::DELETE);
+    }
     /*
     public function configureFields(string $pageName): iterable
     {
