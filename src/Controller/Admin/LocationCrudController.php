3ae17acf697a737221243f6961578f65903dd166
grodeklukasz <lukasz.grodek@gmail.com>
2022-09-20 10:56:50 +0200

    disable delete for Location

--- src/Controller/Admin/LocationCrudController.php
+++ src/Controller/Admin/LocationCrudController.php
@@ -4,12 +4,19 @@ namespace App\Controller\Admin;
 
 use App\Entity\Location;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
 
 class LocationCrudController extends AbstractCrudController
 {
     public static function getEntityFqcn(): string
     {
         return Location::class;
+    }
+
+    public function configureActions(Actions $actions): Actions 
+    {
+        return $actions->disable(Action::DELETE);
     }
 
     /*
