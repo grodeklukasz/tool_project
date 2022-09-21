b6bf0b4270c09976b2be6b1f5f355cd92aac822a
grodeklukasz <lukasz.grodek@gmail.com>
2022-09-21 11:21:19 +0200

    add Lapops Entity in User View

--- src/Controller/BenutzerController.php
+++ src/Controller/BenutzerController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Repository\BenutzerRepository;
 use App\Repository\WorkstationRepository;
+use App\Repository\LaptopRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -14,17 +15,16 @@ class BenutzerController extends AbstractController
     /**
      * @Route("/allItems/{userid}", name="app_user")
      */
-    public function allItemsByUser(String $userid, WorkstationRepository $workstationRepository, BenutzerRepository $benutzerRepository)
+    public function allItemsByUser(String $userid, WorkstationRepository $workstationRepository, LaptopRepository $laptopRepository, BenutzerRepository $benutzerRepository)
     {
         $benutzer = $benutzerRepository->findOneBy(['id' => $userid]);
-        $allWorkstations = $workstationRepository->findBy(
-            [
-                'benutzer' => $userid
-            ]
-        );
+        $allWorkstations = $workstationRepository->findBy(['benutzer' => $userid]);
+        $allLaptops = $laptopRepository->findBy(['benutzer'=>$userid]);
+
         return $this->render('benutzer/index.html.twig', [
             'benutzer' => $benutzer,
             'allWorkstations' => $allWorkstations,
+            'allLaptops' => $allLaptops,
         ]);
     }
 }
